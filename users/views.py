bf452c87ce6382d1f281c69d6d0ddd2bbfc06010
Dmitriy Bychkov <582620@gmail.com>
2023-10-22 17:30:12 +0800

    Add CRUD users controllers

--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer, UserRegisterSerializer
+from users.permissions import IsOwnerOrReadOnly
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    """ Generic - класс для создания (регистрации) нового пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class UserListView(generics.ListAPIView):
+    """ Generic - класс для вывода списка пользователей """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    """ Generic - класс для детального просмотра пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    """ Generic - класс для обновления (редактирования) пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    """ Generic - класс для удаления пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrReadOnly]
