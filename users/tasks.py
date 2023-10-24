67a802455a52acb1581ebc159c7df82467eb33dc
Dmitriy Bychkov <582620@gmail.com>
2023-10-24 20:06:29 +0800

    Rewrite the get_tg_chat_id task logic to get chat_id's correctly and sent to users a welcome message when user write to the chat-bot words /start or /stop

--- users/tasks.py
+++ users/tasks.py
@@ -4,7 +4,7 @@ from users.models import User
 from users.services import tg_get_updates, tg_send_message
 
 
-@shared_task(name="get_tg_chat_id")
+@shared_task(name="get_tg_chat_id_task")
 def get_tg_chat_id():
     """
     Задача на получение и обработку обновлений из Telegram,
@@ -14,28 +14,55 @@ def get_tg_chat_id():
 
     # Получаем данные об обновлениях из Telegram
     tg_data = tg_get_updates()
-    # Получаем пользователей у которых поле chat_id не задано
-    users = User.objects.filter(chat_id=None)
 
-    # Если данные tg_data содержат успешный ответ (ok=True) и список результатов (result) не пустой,
+    # Если данные tg_data содержат успешный ответ (ok=True) и список результатов (result) не пустой
+    # (тоесть пользователь ввел - /start),
     # выполняется цикл для каждого сообщения в tg_data['result'].
-    if tg_data['ok'] and tg_data['result'] != []:
-        for data_dict in tg_data['result']:
-            if data_dict['message']['text'] == "/start":
-                for user in users:
-                    # Проверка на совпадение имени-username в Телеграмм с имененм в модели пользователя
-                    if user.telegram_user_name.lower() == data_dict['message']['from']['username'].lower():
-                        # Если проверка совпадения прошла, тогда вытаскиваем chat_id из данных и присваеваем его пользователю
-                        chat_id = data_dict['message']['from']['id']
-                        user.chat_id = chat_id
-                        user.save()
+    if not tg_data['ok']:
+        print("Ошибка получения обновлений")
+        return
 
-                        # Выполненние функции с message['update_id'] для удаления обработанного обновления.
-                        tg_get_updates(data_dict['update_id'])
+    for data_dict in tg_data['result']:
+        username = data_dict["message"]["chat"]["username"]
+        chat_id = data_dict['message']['chat']['id']
 
-                        # Отправляется сообщение пользователю с приветственным текстом
-                        text = f'Добро пожаловать!\nСюда будут приходить напоминания о ваших привычках!'
-                        tg_send_message(user.chat_id, text)
+        if data_dict['message']['text'] == "/start":
+            try:
+                user = User.objects.get(telegram_user_name=username)
+            except User.DoesNotExist:
+                text = f'Давай досвидания! Тебя нет в нашей базе!'
+                tg_send_message(chat_id, text)
 
-            # Выполненние функции с message['update_id'] для удаления обработанного обновления.
-            tg_get_updates(data_dict['update_id'])
+                # Очищаем все обновления чата
+                tg_get_updates(data_dict['update_id'] + 1)
+                continue
+
+            # Присваиваем пользователю его chat_id
+            if not user.chat_id:
+                user.chat_id = chat_id
+                user.save()
+
+            text = f'Добро пожаловать!\nСюда будут приходить напоминания о ваших привычках!'
+            tg_send_message(chat_id, text)
+
+            # Очищаем все обновления чата
+            tg_get_updates(data_dict['update_id'] + 1)
+
+        elif data_dict['message']['text'] == "/stop":
+            try:
+                user = User.objects.get(telegram_user_name=username)
+            except User.DoesNotExist:
+                text = f'Пользователь не найден!'
+                tg_send_message(chat_id, text)
+
+                # Очищаем все обновления чата
+                tg_get_updates(data_dict['update_id'] + 1)
+                continue
+
+            text = f'Нам очень жаль, что вы решили отписаться от уведомлений:(\nРассылка остановлена!'
+            tg_send_message(chat_id, text)
+            user.chat_id = None
+            user.save()
+
+            # Очищаем все обновления чата
+            tg_get_updates(data_dict['update_id'] + 1)
