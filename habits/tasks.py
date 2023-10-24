d7bde8b83e7d7b90ceae4c532ab221c35c4f919f
Dmitriy Bychkov <582620@gmail.com>
2023-10-24 20:04:15 +0800

    Rewrite the send_habits task logic to sent messages correctly and in time

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,87 +1,54 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
+from django.utils import timezone
 from habits.models import Habit
 from users.services import tg_send_message
 from celery import shared_task
 
 
-# @shared_task(name="send_habits")
-# def send_habits():
-#     """
-#     Задача для асинхронной отправки в телеграмм напоминания о выполнении действия,
-#     чтобы стать в будущем полезной прывычкой
-#     """
-#
-#     current_time = datetime.now()
-#     today = datetime.today().strftime('%A')
-#     for habit in Habit.objects.filter(is_pleasant=False):
-#         if habit.frequency == "Daily" or habit.frequency == today:
-#             if habit.time.strftime("%H:%M") == current_time.strftime("%H:%M"):
-#                 chat_id = habit.owner.chat_id
-#                 if habit.award:
-#                     users_award = habit.award
-#                     message = (
-#                         f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
-#                         f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {users_award}")
-#                     tg_send_message(chat_id, message)
-#                 else:
-#                     users_award = habit.link_pleasant
-#                     message = (f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
-#                                f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {users_award}")
-#                     tg_send_message(chat_id, message)
-#
-#
-# @shared_task(name="send_habits_test")
-# def send_habits_test():
-#     """
-#     Задача для асинхронной отправки в телеграмм напоминания о выполнении действия,
-#     чтобы стать в будущем полезной прывычкой
-#     """
-#
-#     for habit in Habit.objects.filter(is_pleasant=False):
-#
-#         chat_id = habit.owner.chat_id
-#         if habit.award:
-#             users_award = habit.award
-#         else:
-#             users_award = habit.link_pleasant
-#
-#         message = (f"Напоминание от трекера полезных привычек.\nДействие: {habit.action}\nМесто: {habit.place}"
-#                    f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {users_award}")
-#         tg_send_message(chat_id, message)
-
-@shared_task(name="send_habits")
-def send_habits():
+@shared_task(name="send_habits_task")
+def Re():
     """
     Задача для асинхронной отправки в телеграмм напоминания о выполнении действия,
     чтобы стать в будущем полезной привычкой
     """
-    current_time = datetime.now().time()
-    today = datetime.today().strftime('%A')
+
+    # получить текущее дату и время
+    current_dt = timezone.now()
+    # print(f'Текущее время: {current_dt}')
+
     for habit in Habit.objects.filter(is_pleasant=False):
-        if habit.frequency == "Daily" or habit.frequency == today.lower():
-            habit_time = habit.time
-            if is_time_matches(current_time, habit_time):
-                chat_id = habit.owner.chat_id
-                last_reminder_time = habit.last_reminder_time
-                if last_reminder_time is None or (current_time - last_reminder_time) >= timedelta(days=1):
-                    if habit.award:
-                        users_award = habit.award
-                        message = (
-                            f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
-                            f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {users_award}")
-                        tg_send_message(chat_id, message)
-                    else:
-                        users_award = habit.link_pleasant
-                        message = (f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
-                                   f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {users_award}")
-                        tg_send_message(chat_id, message)
-                    # Обновляем время последней отправки напоминания
-                    habit.last_reminder_time = current_time
-                    habit.save()
+        # print(f'Время следующей отправки: {habit.reminder_datetime()}')
 
+        # текущее дата и время >= даты и времени привычки
+        is_reminder_time = current_dt >= habit.reminder_datetime()
+        # print(f'текущее дата и время >= даты и времени привычки?: {is_reminder_time}')
 
-def is_time_matches(current_time, habit_time):
-    """
-    Проверяет, соответствует ли текущее время указанному времени выполнения привычки
-    """
-    return current_time == habit_time
+        # дата и время последнего уведомления < даты и времени напоминания
+        is_last_notify = True
+        if habit.last_reminder_time:
+            is_last_notify = habit.last_reminder_time < habit.reminder_datetime()
+        # print(f'дата и время последнего уведомления < даты и времени напоминания?: {is_last_notify}')
+
+        # текущее дата и время - дата и время напоминания = меньше часа
+        is_right_time = (current_dt - timedelta(hours=1)) < habit.reminder_datetime()
+        # print(f'текущее дата и время - дата и время напоминания = меньше часа?: {is_right_time}')
+
+        if all([is_reminder_time, is_last_notify, is_right_time]):
+            # отправить уведомление
+            if habit.award:
+                # print('1) Привычка, Вознаграждение, Связанная привычка')
+                # print(habit, habit.award, habit.link_pleasant.action)
+                message = (
+                    f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
+                    f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {habit.award}")
+                tg_send_message(habit.owner.chat_id, message)
+            else:
+                # print('2) Привычка, Вознаграждение, Связанная привычка')
+                # print(habit, habit.award, habit.link_pleasant.action)
+                message = (f"Напоминание от трекера привычек!\nДействие: {habit.action}\nМесто: {habit.place}"
+                           f"\nПродолжительность: {habit.duration} мин.\nВаше вознаграждение: {habit.link_pleasant.action}")
+                tg_send_message(habit.owner.chat_id, message)
+
+            # Обновляем время последней отправки напоминания
+            habit.last_reminder_time = current_dt
+            habit.save()
